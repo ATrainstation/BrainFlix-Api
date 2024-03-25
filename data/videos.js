7055a889a90cafbf6ee83812778ce72ed00f3665
ATrainstation <aaronjtrainor@gmail.com>
2024-03-24 20:27:51 -0400

    finish gets

--- data/videos.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-const cors = require('cors');
-const { v4: uuidv4 } = require('uuid');
-const port = process.env.PORT || process.argv[2] || 8080;
-
-app.use(express.json());
-app.use(cors());
--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const vidRoutes = require('./routes/videos.js');
+
+app.use(cors());
+app.use(express.json());
 
 app.get('/', function (req, res) {
     res.send("Fetch the videos");
@@ -9,6 +13,6 @@ app.get('/', function (req, res) {
 app.use('/videos', vidRoutes);
 
 app.listen(8080, function() {
-    console.log('Started server.js, listening on PORT 8080')
+    console.log('Started server "index.js", listening on PORT 8080')
 }
-)
+);
